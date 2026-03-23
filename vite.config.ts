8c757edc86b9476251babb44f5a6cd998546ddc0
Edgars <93377422+Edgars-P@users.noreply.github.com>
2026-03-23 12:48:42 +0200

    Pārvietoti faili priekš build, 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: "index.html",
+        game: "game/index.html",
+        rules: "gamerules/index.html",
+      },
+    },
+  },
 });
